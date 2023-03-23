92b3e4f40e2409bea30bb5d77478b1d1002a695e
Viktoria <vikusya25072000@gmail.com>
2023-03-23 22:18:30 +0300

    решила третью задачу

---
+++ 301/Program.cs
@@ -0,0 +1,41 @@
+﻿int size = 100;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 555);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Мax число: {min}");
+Console.WriteLine($"Мin число: {max}");
+Console.WriteLine($"Разница между маx и мin числами: {max-min}");
